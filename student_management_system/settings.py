6cd1afa72deb3d740673570f9a69c81259a1060b
Fury <qubati212@gmail.com>
2021-12-06 17:45:56 +0600

    heroku

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(i#*06f#keydy_fh17bf=$0f6v)^wr^l7*u4gq42m*sztu#2_m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
